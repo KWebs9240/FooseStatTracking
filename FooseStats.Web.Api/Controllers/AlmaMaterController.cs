e1105ab1a2fa2676f3ea6b216b2f7878d5621317
Kyle Webster <kyle_webster@qbsol.com>
2017-12-11 22:56:07 -0600

    Adding the two new controllers for AlmaMater and Location

---
+++ FooseStats.Web.Api/Controllers/AlmaMaterController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FooseStats.Data.FooseStats.Data.Ef.Entities;
+using FooseStats.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FooseStats.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class AlmaMaterController : Controller
+    {
+        private readonly IBaseDA<AlmaMater> _almaMaterService;
+
+        public AlmaMaterController(IBaseDA<AlmaMater> almaMaterService)
+        {
+            _almaMaterService = almaMaterService;
+        }
+
+        //GetMatches
+        [HttpGet]
+        public IEnumerable<AlmaMater> GetAlmaMaters()
+        {
+            IEnumerable<AlmaMater> rtnList = _almaMaterService.Get();
+
+            return rtnList;
+        }
+
+        //AddMatch
+        [HttpPost]
+        public AlmaMater AddAlmaMater([FromBody]AlmaMater almaMaterToAdd)
+        {
+            if (almaMaterToAdd.AlmaMaterId != null &&
+                _almaMaterService.Get(x => x.AlmaMaterId.Equals(almaMaterToAdd.AlmaMaterId)).Any())
+            {
+                throw new InvalidOperationException("A match type with that Id already exist.");
+            }
+
+            if (almaMaterToAdd.AlmaMaterId.Equals(Guid.Empty))
+            {
+                almaMaterToAdd.AlmaMaterId = Guid.NewGuid();
+            }
+
+            return _almaMaterService.SaveorUpdate(almaMaterToAdd);
+        }
+
+        //UpdateMatch
+        [HttpPut]
+        public AlmaMater UpdateAlmaMater([FromBody]AlmaMater almaMaterToUpdate)
+        {
+            //if (matchTypeToUpdate.MatchTypeId == null
+            //    || !_matchTypeService.GetMatchTypes(x => x.MatchTypeId.Equals(matchTypeToUpdate.MatchTypeId)).Any())
+            //{
+            //    throw new InvalidOperationException("Match type to update does not already exist.");
+            //}
+
+            return _almaMaterService.SaveorUpdate(almaMaterToUpdate);
+        }
+
+        //DeleteMatch
+        [HttpPost]
+        [Route("Delete")]
+        public int DeleteMatchType([FromBody]AlmaMater matchTypeToDelete)
+        {
+            return _almaMaterService.Delete(matchTypeToDelete);
+        }
+    }
+}
---
+++ FooseStats.Web.Api/Controllers/LocationController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FooseStats.Data.FooseStats.Data.Ef.Entities;
+using FooseStats.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FooseStats.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class LocationController : Controller
+    {
+        private readonly IBaseDA<Location> _locationService;
+
+        public LocationController(IBaseDA<Location> locationService)
+        {
+            _locationService = locationService;
+        }
+
+        //GetMatches
+        [HttpGet]
+        public IEnumerable<Location> GetLocations()
+        {
+            IEnumerable<Location> rtnList = _locationService.Get();
+
+            return rtnList;
+        }
+
+        //AddMatch
+        [HttpPost]
+        public Location AddLocation([FromBody]Location locationToAdd)
+        {
+            if (locationToAdd.LocationId != null &&
+                _locationService.Get(x => x.LocationId.Equals(locationToAdd.LocationId)).Any())
+            {
+                throw new InvalidOperationException("A match type with that Id already exist.");
+            }
+
+            if (locationToAdd.LocationId.Equals(Guid.Empty))
+            {
+                locationToAdd.LocationId = Guid.NewGuid();
+            }
+
+            return _locationService.SaveorUpdate(locationToAdd);
+        }
+
+        //UpdateMatch
+        [HttpPut]
+        public Location UpdateMatchType([FromBody]Location locationToUpdate)
+        {
+            //if (matchTypeToUpdate.MatchTypeId == null
+            //    || !_matchTypeService.GetMatchTypes(x => x.MatchTypeId.Equals(matchTypeToUpdate.MatchTypeId)).Any())
+            //{
+            //    throw new InvalidOperationException("Match type to update does not already exist.");
+            //}
+
+            return _locationService.SaveorUpdate(locationToUpdate);
+        }
+
+        //DeleteMatch
+        [HttpPost]
+        [Route("Delete")]
+        public int DeleteLocation([FromBody]Location locationToDelete)
+        {
+            return _locationService.Delete(locationToDelete);
+        }
+    }
+}
